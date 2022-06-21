a13411e1feb1051b77ba3de8a28266687858165c
Pablo Fernandez <pfer@me.com>
2022-06-21 21:26:51 +0300

    Revert "change port to 3000"
    This reverts commit e4215b6da8468c3a50a479d3f3b5d70b13f332c7.

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
-  public static stsAuthority = 'https://pblsw-api.ngrok.io/oidc';
+  public static stsAuthority = 'https://api.dev.swanbitcoin.com/oidc';
   public static clientId = 'oidc-client-test';
   public static clientSecret = 'btO@z7lBWHgX^cvwj5XW1T7%N@FFXuh!R6ZeU^H7*ereCB$#6FpNYOMq*N^w4Qr4';
-  public static clientRoot = 'http://localhost:3000/';
+  public static clientRoot = 'http://localhost:3001/';
   public static clientScope = 'offline_access openid v1 read:vendor_wallet write:vendor_wallet read:automatic_withdrawal write:automatic_withdrawal';
-  public static apiRoot = 'https://pblsw-api.ngrok.io/apps/v20210824/';
+  public static apiRoot = 'https://api.dev.swanbitcoin.com/apps/v20210824/';
 }
